ba798f4d44dda757b254f6dc2cfc339d5c97c36b
Brandon Harvey (bbook) <brandon@oblong.com>
2016-11-30 17:22:13 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# spaceconvert
+
+Convert from room + feld + (optionally) room proteins ==> space definition
+
+# Usage
+
+        ./spaceconvert.js [options]
+
+
+```
+Options
+
+  --screen string   A screen.protein file, normal or multi-machine style.                         
+                    Example: https://gist.github.com/sandover/247d9acb9e33769813b2ba55d592a53e    
+  --feld string     A feld.protein file, normal or kombi-feld style.                              
+                    Example: https://gist.github.com/sandover/1f306bba2e70d1fbe6040f22920a7d39    
+  --room string     A room.protein file (optional).                                               
+                    Example: https://gist.github.com/sandover/b8a1ca0b039c8d882a9e3e98a2e6ea42    
+```
+
---
+++ spaceconvert.js
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
+
+
+const protein = require('proteinslurp');
+const space = require('spacedef');
+
+
+// -----------------------------
+// Parse command line args
+// -----------------------------
+const commandLineArgs = require('command-line-args');
+const getUsage = require('command-line-usage');
+var cmd_line_options;
+const optionDefinitions = [{
+    name: 'screen',
+    type: String,
+    multiple: false,
+    description: 'A screen.protein file, normal or multi-machine style.\nExample: https://gist.github.com/sandover/247d9acb9e33769813b2ba55d592a53e'
+}, {
+    name: 'feld',
+    type: String,
+    multiple: false,
+    description: 'A feld.protein file, normal or kombi-feld style.\nExample: https://gist.github.com/sandover/1f306bba2e70d1fbe6040f22920a7d39'
+}, {
+    name: 'room',
+    type: String,
+    multiple: false,
+    description: 'A room.protein file (optional).\nExample: https://gist.github.com/sandover/b8a1ca0b039c8d882a9e3e98a2e6ea42'
+}];
+
+
+const usage_doc_sections = [{
+    header: 'convert',
+    content: 'Convert from room + feld + (optionally) room proteins ==> space definition'
+}, {
+    header: 'Options',
+    optionList: optionDefinitions
+}]
+const usage_doc = getUsage(usage_doc_sections)
+
+
+try {
+    cmd_line_options = commandLineArgs(optionDefinitions)
+} catch (err) {
+    if (err.name != "UNKNOWN_OPTION") {
+        console.log(usage_doc);
+        throw (err);
+    }
+}
+
+if (!cmd_line_options.feld || !cmd_line_options.screen) {
+    console.log(usage_doc);
+} else {
+
+    let room_protein, screen_protein, feld_protein;
+
+    if (cmd_line_options.room) {
+        protein.slurp_and_protein_parse(cmd_line_options.room, p => room_protein = p);
+    }
+    else { 
+        room_protein = space.default_room();
+    }
+
+    protein.slurp_and_protein_parse(cmd_line_options.screen, 
+        p => screen_protein = p);
+    protein.slurp_and_protein_parse(cmd_line_options.feld, 
+        p => feld_protein = p);
+
+    let s = space.convert_roomfeldscreen_to_space(room_protein, feld_protein,
+        screen_protein);
+    console.dir(s, {depth: null});
+}
